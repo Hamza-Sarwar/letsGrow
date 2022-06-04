52a2faaffbcd73c7f3c66917bebc0e61f8b9c76b
Hamza-Sarwar <hamzasarwar129@gmail.com>
2022-06-04 17:54:39 +0500

    Set Debug True again

--- LetsGrow/settings.py
+++ LetsGrow/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g)yf3rb#klf)%fx7^2$x60fy@vg%96_=goqjx!1#_hbikm!n)o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['letsgrow.one', 'www.letsgrow.one','localhost', '127.0.0.1' ]
 
