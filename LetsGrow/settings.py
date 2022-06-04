2427910c210060127e4c8047b90cc5c332411019
Hamza-Sarwar <hamzasarwar129@gmail.com>
2022-06-04 18:40:16 +0500

    Static_DIrs Activated

--- LetsGrow/settings.py
+++ LetsGrow/settings.py
@@ -118,13 +118,8 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/' # the path in url
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
